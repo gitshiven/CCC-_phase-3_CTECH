eafb34fb1a6fa7857ba469501d8fb16445ba7a84
Shiven Singh <91045496+gitshiven@users.noreply.github.com>
2023-03-06 16:13:58 +0530

    Update R 105 : Mice reach the Home.py
    Run in python 3

--- R 105 : Mice reach the Home.py
+++ R 105 : Mice reach the Home.py
@@ -1,10 +1,10 @@
-n = int(input())
+s = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 a.sort()
 b.sort()
  
 res = 0
-for i in range(n):
+for i in range(s):
   if abs(a[i] - b[i]) > res: res = abs(a[i] - b[i])
 print(res)
