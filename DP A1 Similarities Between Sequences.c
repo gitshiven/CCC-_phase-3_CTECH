5c7bb3f4c06bc8bed8f57ab42208a11c34eb2d30
Shiven Singh <91045496+gitshiven@users.noreply.github.com>
2023-03-06 18:14:58 +0530

    Update DP A1 Similarities Between Sequences.c

--- DP A1 Similarities Between Sequences.c
+++ DP A1 Similarities Between Sequences.c
@@ -35,3 +35,23 @@ int main() {
     printf("%d\n", maxLen);
     return 0;
 }
+
+//python code(might work)
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+    
+dp = [[0] * (m+1) for _ in range(n+1)]
+
+maxLen = 0
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+            maxLen = max(maxLen, dp[i][j])
+        else:
+            dp[i][j] = 0
+
+print(maxLen)
+
